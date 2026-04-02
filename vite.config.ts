c687e2854f22dffe677223f671f9981f72c03aea
foodie360 <alvaro@disartmedia.com>
2026-04-02 18:42:19 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -154,8 +154,7 @@ const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(
 
 export default defineConfig({
   plugins,
-    base: '/miamidaderesearch/',
-  resolve: {
+    solve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
       "@shared": path.resolve(import.meta.dirname, "shared"),
